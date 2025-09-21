2b73adf0294cf1a91a615224ab14e1777a9535c6
Shaik Rahil <rahil.ahmed.sk@gmail.com>
2025-09-22 00:31:29 +0530

    a chnages

--- src/app/component/Nav.js
+++ src/app/component/Nav.js
@@ -1,28 +1,28 @@
-"use client";
+
 import React from "react";
-import Link from "next/link";
+
 
 const Nav = () => {
   return (
     <nav>
       <div className="flex gap-6 items-center">
-        <Link href="/" className="hover:text-primary transition">
+        <a href="/" className="hover:text-primary transition">
           Home
-        </Link>
-        <Link href="/about" className="hover:text-primary transition">
+        </a>
+        <a href="/about" className="hover:text-primary transition">
           About
-        </Link>
-        <Link href="/features" className="hover:text-primary transition">
+        </a>
+        <a href="/features" className="hover:text-primary transition">
           Features
-        </Link>
-        <Link href="/contact" className="hover:text-primary transition">
+        </a>
+        <a href="/contact" className="hover:text-primary transition">
           Contact
-        </Link>
-        <Link href="/getStarted">
+        </a>
+        <a href="/getStarted">
           <button className="px-5 py-2 bg-blue-500 text-white rounded-lg shadow hover:scale-105 transition">
             Get Started
           </button>
-        </Link>
+        </a>
       </div>
     </nav>
   );
