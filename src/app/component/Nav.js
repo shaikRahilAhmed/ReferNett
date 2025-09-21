d4d70b2190c46cb9ce5812df5480f5ae15d64d75
Shaik Rahil <rahil.ahmed.sk@gmail.com>
2025-09-22 00:14:41 +0530

    Intial commit

--- src/app/component/Nav.js
+++ src/app/component/Nav.js
@@ -1,30 +1,31 @@
-import React from 'react'
+"use client";
+import React from "react";
+import Link from "next/link";
 
 const Nav = () => {
   return (
     <nav>
-        <div className="flex gap-6 items-center">
-            <a href="/" className="hover:text-primary transition">
-            Home
-          </a>
-          <a href="/about" className="hover:text-primary transition">
-            About
-          </a>
-          <a href="/features" className="hover:text-primary transition">
-            Features
-          </a>
-          <a href="/contact" className="hover:text-primary transition">
-            Contact
-          </a>
-          <a href='/getStarted'>
+      <div className="flex gap-6 items-center">
+        <Link href="/" className="hover:text-primary transition">
+          Home
+        </Link>
+        <Link href="/about" className="hover:text-primary transition">
+          About
+        </Link>
+        <Link href="/features" className="hover:text-primary transition">
+          Features
+        </Link>
+        <Link href="/contact" className="hover:text-primary transition">
+          Contact
+        </Link>
+        <Link href="/getStarted">
           <button className="px-5 py-2 bg-blue-500 text-white rounded-lg shadow hover:scale-105 transition">
             Get Started
-          </button></a>
-        </div>
+          </button>
+        </Link>
+      </div>
     </nav>
+  );
+};
 
-    
-  )
-}
-
-export default Nav
+export default Nav;
